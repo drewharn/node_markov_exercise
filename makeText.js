dc9dbf27d822bdb5f981b459e97ca16dd628f204
Andrew <andrew.harnedy@gmail.com>
2024-05-27 16:36:02 -0400

    mark

---
+++ makeText.js
@@ -0,0 +1,7 @@
+/** Command-line tool to generate Markov text. */
+
+
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process")
